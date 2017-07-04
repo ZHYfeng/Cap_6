f097c9ff025cb45ab98f820c2087fe9e4bfee5d0
unknown <haoyu.zz@foxmail.com>
2017-07-04 15:42:38 +0800

    ..

--- lib/Encode/ListenerService.cpp
+++ lib/Encode/ListenerService.cpp
@@ -666,9 +666,9 @@ namespace klee {
 			rdManager.allPTSCost.push_back(cost);
 
 			int size;
-			rdManager.Send_Data.push_back(trace->Send_Data.size);
+			rdManager.Send_Data.push_back(trace->Send_Data_Expr.size);
 			size = 0;
-			for(std::vector<std::string>::iterator it = trace->Send_Data_Expr.begin(), ie = trace->Send_Data_Expr.end(); it != ie; it++) {
+			for(std::set<std::string>::iterator it = trace->Send_Data_Expr.begin(), ie = trace->Send_Data_Expr.end(); it != ie; it++) {
 				for (std::vector<std::string>::iterator itt = trace->DTAMSerial.begin(), iee = trace->DTAMSerial.end(); itt != iee; itt++) {
 					if ((*itt) == (*it)) {
 						size++;
@@ -676,7 +676,7 @@ namespace klee {
 				}
 			}
 			rdManager.Send_Data_Serial.push_back(size);
-			for(std::vector<std::string>::iterator it = trace->Send_Data_Expr.begin(), ie = trace->Send_Data_Expr.end(); it != ie; it++) {
+			for(std::set<std::string>::iterator it = trace->Send_Data_Expr.begin(), ie = trace->Send_Data_Expr.end(); it != ie; it++) {
 				for (std::vector<std::string>::iterator itt = trace->taintPTS.begin(), iee = trace->taintPTS.end(); itt != iee; itt++) {
 					if ((*itt) == (*it)) {
 						size++;
@@ -686,7 +686,7 @@ namespace klee {
 			rdManager.Send_Data_PTS.push_back(size);
 
 			size = 0;
-			for(std::vector<std::string>::iterator it = trace->Send_Data_Expr.begin(), ie = trace->Send_Data_Expr.end(); it != ie; it++) {
+			for(std::set<std::string>::iterator it = trace->Send_Data_Expr.begin(), ie = trace->Send_Data_Expr.end(); it != ie; it++) {
 				for (std::vector<std::string>::iterator itt = trace->DTAMParallel.begin(), iee = trace->DTAMParallel.end(); itt != iee; itt++) {
 					if ((*itt) == (*it)) {
 						size++;
@@ -696,7 +696,7 @@ namespace klee {
 			rdManager.Send_Data_Parallel.push_back(size);
 
 			size = 0;
-			for(std::vector<std::string>::iterator it = trace->Send_Data_Expr.begin(), ie = trace->Send_Data_Expr.end(); it != ie; it++) {
+			for(std::set<std::string>::iterator it = trace->Send_Data_Expr.begin(), ie = trace->Send_Data_Expr.end(); it != ie; it++) {
 				for (std::vector<std::string>::iterator itt = trace->DTAMhybrid.begin(), iee = trace->DTAMhybrid.end(); itt != iee; itt++) {
 					if ((*itt) == (*it)) {
 						size++;
